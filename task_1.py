c19b2bbdb1b5149ef55b902cfff2b4bfc5df0d4c
LeoC95188 <leoc79915@gmail.com>
2024-06-17 10:40:09 -0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,3 +1,17 @@
-first_name = input("What's your first name? ")
-last_name = input("Whats your last name? ")
-print("Welcome to task 1")
+first_name = input("What's your first name?")
+last_name = input("Whats your last name?")
+print("Welcome" + first_name + last_name)
+print("This quiz help you choose which dog breed might suit you best")
+name = first_name + last_name  
+print ("Let's begin") 
+gut = input('Do you prefer a fluffy dog over a smooth haired dog? [Please answer with yes or no] ')
+if gut == "yes" or "Yes":
+  print("alright" + first_name)
+else:
+  print("please answer the question with a yes or no")
+answer = input("Do you like a larger companion over a smaller one ")
+if answer == "yes":
+  print("I would have said the same.")
+else:
+  print("Ok then")
+input("Do you currently have any pets [yes or no]")
